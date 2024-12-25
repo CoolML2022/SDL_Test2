5379a2d80a35f5abfe3702e5792cad1b95cde6c5
CoolML2022 <ich@vincentkrebs.de>
2024-12-25 16:39:48 +0100

    Verbesserte Perfomance, indem Alle rechtecke aufeinmal gezeichnet werden

--- SDL_Test2/main.cpp
+++ SDL_Test2/main.cpp
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <unordered_map>
 #include <cstring>
+#include <thread>
 // Wrapper für das Fenster (automatische Auflösung, beim verlassen der Main Methode)
 
 const int WINDOW_WIDTH = 800;
@@ -18,14 +19,13 @@ const int WINDOW_HEIGHT = 600;
 const int CELL_SIZE = 20;
 const int ROWS = WINDOW_HEIGHT / CELL_SIZE;
 const int COLUMNS = WINDOW_WIDTH / CELL_SIZE;
+const SDL_Color SandColor = {255, 204, 98, 100};
 
-struct pair_hash {
-    template <class T1, class T2>
-    std::size_t operator() (const std::pair<T1,T2>& p) const {
-        auto hash1 = std::hash<T1>{}(p.first);
-        auto hash2 = std::hash<T2>{}(p.second);
-        return hash1 ^ hash2; // XOR-Kombination der Hashes
-    }
+struct Cell {
+    bool isFilled;
+    float x_velocity;
+    float y_velocity;
+    SDL_Color color;
 };
 
 void SDL_DrawGrid(SDL_Renderer *renderer) {
@@ -37,57 +37,72 @@ void SDL_DrawGrid(SDL_Renderer *renderer) {
     }
     
 }
-void DrawCells(SDL_Renderer *renderer, int (*cells)[ROWS] ) {
+void DrawCells(SDL_Renderer *renderer, Cell *cells) {
+    std::vector<SDL_Rect> rectangles;
     for (int i = 0; i < COLUMNS; i++) {
         for (int j = 0; j < ROWS; j++) {
-            if (cells[i][j] != 0) {
-                //SDL_DrawRect(renderer, i * CELL_SIZE, j * CELL_SIZE);
-                //std::cout << "rendered cell, at: " << i << j << std::endl;
+            int index = j * COLUMNS + i;
+            if (cells[index].isFilled) {
                 SDL_Rect rect = {i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE};
-                SDL_SetRenderDrawColor(renderer, 255, 242, 172, 100);
-                SDL_RenderFillRect(renderer, &rect);
+                rectangles.push_back(rect);
             }
         }
     }
-}
-int getRandomSign() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 1);
-    int randomValue = dis(gen);
-    return (randomValue == 0) ? -1 : 1;
+    SDL_SetRenderDrawColor(renderer, 255, 204, 98, 100);
+    SDL_RenderFillRects(renderer, rectangles.data(), static_cast<int>(rectangles.size()));
 }
 
-void UpdateCells(int (*cells)[ROWS]) {
-    int nextCells[COLUMNS][ROWS] = { 0 };
-    for (int i = 0; i < COLUMNS - 1; i++) {
+const int LUT_SIZE = 2;  // Größe des Lookup Tables
+const int randomSigns[LUT_SIZE] = {-1, 1};
+int getRandomSign() {
+    int index = std::rand() % LUT_SIZE;
+    return randomSigns[index];
+}
+void UpdateCells(Cell* cells,Cell* nextCells, int start_i, int end_i) {
+    for (int i = start_i; i < end_i; i++) {
         for (int j = 0; j < ROWS; j++) {
-            if(cells[i][j] == 0) {
+            // Berechnung des Indexes im eindimensionalen Array
+            int index = j * COLUMNS + i;
+            Cell* currentCell = &cells[index];
+            
+            // Skipping this cell, if there is nothing inside
+            if (!cells[index].isFilled) {
                 continue;
             }
-            if (j < ROWS - 1 && cells[i][j+1] == 0) {
-                nextCells[i][j+1] = 1;
-                cells[i][j] = 0;
+            //std::cout << "cell: " << i << "," << j << " not empty!" << std::endl;
+            // Checking if it is touching the floor
+            const int lastRow = ROWS - 1;
+            if (j == lastRow) {
+                nextCells[index] = *currentCell;
+                continue;
+            }
+            
+            int downIndex = (j + 1) * COLUMNS + i;  // Index der Zelle direkt unter der aktuellen Zelle
+            
+            if (!cells[downIndex].isFilled) {
+                nextCells[downIndex] = *currentCell;
             } else {
-                bool isBelowLeftFree = (j < ROWS - 1 && i > 0) && cells[i - 1][j + 1] == 0;
-                bool isBelowRightFree = (j < ROWS - 1 && i < COLUMNS - 2) && cells[i + 1][j + 1] == 0;
-                
-                if(isBelowLeftFree && isBelowRightFree) {
-                    nextCells[i + getRandomSign()][j +1] = 1;
-                    cells[i][j] = 0;
-                } else if (isBelowLeftFree) {
-                    nextCells[i - 1][j +1] = 1;
-                    cells[i][j] = 0;
-                } else if (isBelowRightFree) {
-                    nextCells[i + 1][j +1] = 1;
-                    cells[i][j] = 0;
+                int di = 0;  // Horizontaler Offset
+                if (i > 0 && !cells[(j + 1) * COLUMNS + (i - 1)].isFilled) {
+                    di = -1;  // Bewegung nach unten links
+                    //std::cout << "Down Left i: " << i << " start: " << start_i << " end: " << end_i << std::endl;
+                }
+                if (i < COLUMNS - 1 && !cells[(j + 1) * COLUMNS + (i + 1)].isFilled) {
+                    if (di == -1) {
+                        di = getRandomSign();  // Zufällige Bewegung, wenn beide Richtungen frei sind
+                    } else {
+                        di = 1;  // Bewegung nach unten rechts
+                    }
+                }
+                if (di != 0) {
+                    nextCells[(j + 1) * COLUMNS + (i + di)] = *currentCell;  // Bewegung der Zelle
+                } else {
+                    nextCells[index] = *currentCell;  // Zelle bleibt an Ort und Stelle
                 }
             }
         }
     }
-    std::memcpy(cells, nextCells, sizeof(nextCells));
 }
-
 
 int main(int argc, const char *argv[]) {
     // SDL initialisieren
@@ -114,12 +129,17 @@ int main(int argc, const char *argv[]) {
     }
     
     // Setting all Cells empty
-    int cells[COLUMNS][ROWS] = { 0 };
+    Cell cells[COLUMNS * ROWS] = {false, 0, 0, SDL_Color{0, 0, 0, 0} };
+    Cell nextCells[COLUMNS * ROWS] = {false, 0, 0, SDL_Color{0, 0, 0, 0} };
+    int numOfThreads = 4; // Anzahl der Threads
+    std::vector<std::thread> threads;  // Vektor für die Threads
     bool mouseButtonPressed = false;
     bool quit = false;
-    bool stepTriggered = false;
     SDL_Event event;
-    
+    int frameCount = 0;
+    Uint32 last_tick_time = 0;
+    Uint32 delta = 0;
+    Uint32 lastTime = SDL_GetTicks();
     while(!quit) {
         // events handeling
         while (SDL_PollEvent(&event)) {
@@ -133,23 +153,10 @@ int main(int argc, const char *argv[]) {
             if (event.type == SDL_MOUSEBUTTONUP) {
                 mouseButtonPressed = false;
             }
-            if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_SPACE && !stepTriggered) {
-                    stepTriggered = true; // Nur ein Schritt pro Tastendruck
-                }
-                if (event.key.keysym.sym == SDLK_q) {
-                    quit = true; // Programm beenden
-                }
-            }
-            if (event.type == SDL_KEYUP) {
-                if (event.key.keysym.sym == SDLK_SPACE) {
-                    stepTriggered = false; // Zurücksetzen, wenn die Taste losgelassen wird
-                }
-            }
             if ((event.type == SDL_MOUSEMOTION && event.motion.state != 0) || mouseButtonPressed) {
                 int cell_x = event.motion.x / CELL_SIZE;
                 int cell_y = event.motion.y / CELL_SIZE;
-                int range = 1; // Dies bestimmt, wie viele Zellen um die Maus herum ausgefüllt werden
+                int range = 0; // Dies bestimmt, wie viele Zellen um die Maus herum ausgefüllt werden
                 // Schleife über die Zellen rund um die Maus
                 for (int i = -range; i <= range; ++i) {
                     for (int j = -range; j <= range; ++j) {
@@ -158,31 +165,64 @@ int main(int argc, const char *argv[]) {
                         int target_y = cell_y + j;
                         
                         // Überprüfe, ob die Zelle innerhalb des Arrays liegt
-                        if (target_x >= 0 && target_x < COLUMNS && target_y >= 0 && target_y < ROWS) {
-                            cells[target_x][target_y] = 1;  // Setze das Zellenfeld auf 1
+                        if (target_x >= 0 && target_x < COLUMNS && target_y >= 0 && target_y < ROWS && getRandomSign() < 0) {
+                            int index = target_y * COLUMNS + target_x;
+                            cells[index] = {true, 0, 1, SandColor};  // Setze das Zellenfeld auf 1
                         }
                     }
                 }
-                
             }
         }
         
-        UpdateCells(cells);
+        // Delta Time
+        Uint32 tick_time = SDL_GetTicks();
+        delta = tick_time - last_tick_time;
+        last_tick_time = tick_time;
+        //std::cout << "Delta-time: " << delta << std::endl;
+        
+        //FrameRate
+        frameCount++;
+        if (tick_time - lastTime >= 1000) {  // Alle 1000 ms (1 Sekunde)
+            float fps = frameCount / ((tick_time - lastTime) / 1000.0f);
+            std::cout << "  FPS: " << fps << std::endl;
+            frameCount = 0;
+            lastTime = tick_time;
+        }
+        
+        
+        
+        // rendering
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
         DrawCells(renderer, cells);
         SDL_RenderPresent(renderer);
-        SDL_Delay(32);
         
-        //SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-        //SDL_DrawGrid(renderer);
-        
-        
-        // rendering
-        
-        // update
-        
+        // update (multithreaded)
+        for (int t = 0; t < delta/8; t++) {
+            threads.clear();
+            if (numOfThreads > 1) {
+                int columnsPerThread = COLUMNS / numOfThreads;
+                threads.emplace_back(UpdateCells, std::ref(cells), std::ref(nextCells), 0, columnsPerThread);
+                
+                for (int i = 1; i < numOfThreads; i++) {
+                    threads.emplace_back(UpdateCells, std::ref(cells), std::ref(nextCells), i * (columnsPerThread), i * columnsPerThread + columnsPerThread);
+                }
+            } else if (numOfThreads == 1){
+                threads.emplace_back(UpdateCells, std::ref(cells), std::ref(nextCells), 0, COLUMNS);
+            } else {
+                // Falls kein Multithreading genutzt wird
+                UpdateCells(std::ref(cells), std::ref(nextCells), 0, COLUMNS);
+            }
+            
+            
+            
+            for (auto& t : threads) {
+                t.join();
+            }
+            //UpdateCells(std::ref(cells), std::ref(nextCells), 0, COLUMNS);
+            std::copy(nextCells, nextCells + COLUMNS * ROWS, cells);
+            std::memset(&nextCells[0], 0, sizeof(nextCells));
+        }
     }
     return 0;
 }
-
